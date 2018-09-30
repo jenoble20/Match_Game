8bba51604b1bfe4fd63ba0c9d17632dd33ec3049
jenoble <jenoble20@gmail.com>
2018-09-30 13:07:15 -0400

    Functional match game.

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -37,17 +37,21 @@ MatchGame.renderCards = function(cardValues, $game) {
   const cardColors = ['hsl(25,85%,65%)','hsl(55,85%,65%)','hsl(90,85%,65%)',
                       'hsl(160,85%,65%)','hsl(220,85%,65%)','hsl(265,85%,65%)',
                       'hsl(310,85%,65%)','hsl(360,85%,65%)'];
+
+
   for(let i=0; i<cardValues.length; i++){
     let $card = $('<div class="card col-lg-3"></div>');
-    $card.data('value', cardValues[i]);
-    $card.data('flipped', false);
-    $card.data('color', cardColors[cardValues[i]-1]);
+    $card.data({
+      value: cardValues[i],
+      flipped: false,
+      color: cardColors[cardValues[i]-1]
+    });
     $game.append($card);
   }
-  jquery('$game > div').click((, $game) => {
-    MatchGame.flipCard($card, $game);
-  })
-};
+  $('.card').click(function() {
+    MatchGame.flipCard($(this), $('#game-board'));
+  });
+}
 
 /*
   Flips over a given card and checks to see if two cards are flipped over.
@@ -55,37 +59,39 @@ MatchGame.renderCards = function(cardValues, $game) {
  */
 
 MatchGame.flipCard = function($card, $game) {
-  if($card.data("flipped")===true){
+  console.log($card);
+  if($card.data("flipped")){
+    console.log('TRUE');
     return
   }
+
   $card.text($card.data('value'));
   $card.css('background-color', $card.data('color'));
   $card.data('flipped', true);
 
   $game.data('flipped-cards').push($card);
-  if($game.data('flipped-card').length === 2){
-    let cardOne = $game.data('flipped-card')[0];
-    let cardTwo = $game.data('flipped-card')[1]
+  if($game.data('flipped-cards').length === 2){
+    let cardOne = $game.data('flipped-cards')[0];
+    let cardTwo = $game.data('flipped-cards')[1];
+    console.log(cardOne);
     if(cardOne.data('value')===cardTwo.data('value')){
       cardOne.css({
-        'background-color': rgb(153,153,153),
-        color: rgb(204,204,204)
+        'background-color': 'rgb(153,153,153)',
+        color: 'rgb(204,204,204)'
       });
       cardTwo.css({
-        'background-color': rgb(153,153,153),
-        color: rgb(204,204,204)
+        'background-color': 'rgb(153,153,153)',
+        color: 'rgb(204,204,204)'
       });
     }else{
-        cardOne.css({
-          'background-color': rgb(32,64,86),
-        });
+      window.setTimeout(function(){
+        cardOne.css('background-color', 'rgb(32,64,86)');
         cardOne.text('');
         cardOne.data('flipped', false);
-        cardTwo.css({
-          'background-color': rgb(32,64,86),
-        });
+        cardTwo.css('background-color', 'rgb(32,64,86)');
         cardTwo.text('');
         cardTwo.data('flipped', false);
+      },500)
     }
     $game.data('flipped-cards', []);
   }
